ecf0263b64f67bf42576f5e6636e93ad529bab4e
sebaspz <sebaspz@gmail.com>
2021-09-21 08:27:25 -0500

    Se elimino tiburon de nuevo

--- src/Vertebrado.java
+++ src/Vertebrado.java
@@ -2,7 +2,7 @@ public class Vertebrado {
    protected String nombreVertebrado;  
    protected String claseVertebrado; // valores:mamífero, ave, pez, reptil, anfibio
    
-   
+   // rompedor
    public Vertebrado(String nombreVertebrado, String claseVertebrado){
        this.nombreVertebrado = nombreVertebrado;
        this.claseVertebrado = claseVertebrado;
